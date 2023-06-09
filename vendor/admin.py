13cffe5950b3e9b356bd63ffc4106bbe34c9f041
Maxim Manzik <kepkpkep@gmail.com>
2023-06-09 20:17:44 +0300

    forgot password feature and send email activation

--- vendor/admin.py
+++ vendor/admin.py
@@ -5,7 +5,7 @@ from .models import Vendor
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ('vendor_name', 'user', 'user_profile')
+    list_display = ('vendor_name', 'user', 'user_profile', 'is_approved')
     list_display_links = ('user', 'vendor_name')
 
 
